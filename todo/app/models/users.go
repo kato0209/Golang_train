0f0baabff6adedc6d28d6d1acf3c44b701d98e3b
Your Name <kato.921002@gmail.com>
2023-09-03 18:23:53 +0900

    todo finish

--- todo/app/models/users.go
+++ todo/app/models/users.go
@@ -12,6 +12,15 @@ type User struct {
 	Email     string
 	PassWord  string
 	createdAt time.Time
+	Todos     []Todo
+}
+
+type Session struct {
+	ID        int
+	UUID      string
+	Email     string
+	UserID    int
+	createdAt time.Time
 }
 
 func (u *User) CreateUser() (err error) {
@@ -63,4 +72,70 @@ func (u *User) DeleteUser() (err error) {
 	}
 
 	return err
+}
+
+func GetUserByEmail(email string) (user User, err error) {
+	user = User{}
+	cmd := `select id, uuid, name, email, password, created_at from users where email = ?`
+
+	err = Db.QueryRow(cmd, email).Scan(&user.ID, &user.UUID, &user.Name, &user.Email, &user.PassWord, &user.createdAt)
+
+	return user, err
+}
+
+func (u *User) CreateSession() (session Session, err error) {
+	session = Session{}
+	cmd1 := `insert into sessions (
+		uuid,
+		email,
+		user_id,
+		created_at) values (?, ?, ?, ?)`
+
+	_, err = Db.Exec(cmd1, createUUID(), u.Email, u.ID, time.Now())
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cmd2 := `select id, uuid, email, user_id, created_at from sessions where user_id = ? and email = ?`
+
+	err = Db.QueryRow(cmd2, u.ID, u.Email).Scan(&session.ID, &session.UUID, &session.Email, &session.UserID, &session.createdAt)
+
+	return session, err
+}
+
+func (sess *Session) CheckSession() (valid bool, err error) {
+	cmd := `select id, uuid, email, user_id, created_at from sessions where uuid = ?`
+
+	err = Db.QueryRow(cmd, sess.UUID).Scan(&sess.ID, &sess.UUID, &sess.Email, &sess.UserID, &sess.createdAt)
+
+	if err != nil {
+		valid = false
+		return valid, err
+	}
+	if sess.ID != 0 {
+		valid = true
+	}
+	return valid, err
+}
+
+func (sess *Session) DeleteSessionByUUID() (err error) {
+	cmd := `delete from sessions where uuid = ?`
+
+	_, err = Db.Exec(cmd, sess.UUID)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return err
+}
+
+func (sess *Session) GetUserBySession() (user User, err error) {
+	user = User{}
+	cmd := `select id, uuid, name, email, password, created_at from users where id = ?`
+
+	err = Db.QueryRow(cmd, sess.UserID).Scan(&user.ID, &user.UUID, &user.Name, &user.Email, &user.PassWord, &user.createdAt)
+
+	return user, err
 }
--- todo/main.go
+++ todo/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"Golang_train/todo/app/models"
+	"Golang_train/todo/app/controllers"
 	_ "Golang_train/todo/app/models"
 	_ "Golang_train/todo/config"
 )
 
 func main() {
-	u, _ := models.GetUser(1)
-
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
+	controllers.StartMainServer()
 }
